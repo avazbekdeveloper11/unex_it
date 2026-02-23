f87888bc1b327c4b52ebdf20554916e2b52a37cc
avazbek <avazbekdevleoper11@gmail.com>
2026-02-23 22:20:04 +0500

    push fixed bug

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
   title: 'UNEX IT | Premium IT Solutions',
   description: 'Mobile App, CRM, AI and Website Development',
 }
-
+// app/layout.js
 export default function RootLayout({ children }) {
   return (
     <html lang="uz">
